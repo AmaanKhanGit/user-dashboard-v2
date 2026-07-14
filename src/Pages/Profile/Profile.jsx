d8adb75510bf423108efe9cd92e027e86c4b2ba0
AmaanKhanGit <amaankhan050606@gmail.com>
2026-07-14 19:03:55 +0530

    minimal changes

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -29,8 +29,8 @@ const Profile = () => {
   return (
     <DashboardLayout className="flex flex-col gap-3 px-4 py-4 lg:grid lg:grid-cols-2">
       <ProfileHero className="col-span-2" />
-      <AboutMe />
       <PersonalInfo className="row-span-2" />
+      <AboutMe />
       <Skills />
     </DashboardLayout>
   );
