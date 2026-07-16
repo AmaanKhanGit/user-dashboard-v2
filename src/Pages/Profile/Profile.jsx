1d578f16e59b657c7152361072346eeed812c102
AmaanKhanGit <amaankhan050606@gmail.com>
2026-07-16 13:11:21 +0530

    padding adjust

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -6,7 +6,7 @@ import PersonalInfo from "./components/PersonalInfo";
 
 const Profile = () => {
   return (
-    <DashboardLayout className="flex flex-col gap-3 px-4 py-4 lg:grid lg:grid-cols-2">
+    <DashboardLayout className="flex flex-col gap-3 px-1 py-4 sm:px-4 lg:grid lg:grid-cols-2">
       <ProfileHero className="col-span-2" />
       <PersonalInfo className="row-span-2" />
       <AboutMe />
